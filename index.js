0a1f1ea76a3670617d4cc66d07b00135ed65d633
Saurav Khanal <hi.sauravkhanal@gmail.com>
2023-11-24 16:02:18 +0545

    unifies js file as error occured while using import will fix later

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-import {infer} from 'infer.js'
-import {handlePrediction} from "./handle prediction.js";
-
+// import {infer} from './infer.js'
+// import {handlePrediction} from "./handle_prediction.js";
 function handleClick(event) {
     //prevent page reload, infer, show predictions
     //prevent page reload
     event.preventDefault();
     alert("clicked");
-    
+
     //show inferring... text
     const result = document.getElementById('loading');
     result.style.display = 'block';
-    
+
     //access uploaded image
     const inpFile = document.getElementById('image-picker')
     const inpImage = inpFile.files[0];
-    
+
     //create FormData object to send via POST
     const formData = new FormData();
-    formData.append('image', inpImage)
-    
+    formData.append('image', inpImage);
+
     //now infer if user has uploaded image and clicked infer btn
     if (inpImage) {
 
@@ -28,18 +27,38 @@ function handleClick(event) {
         const prediction = infer(formData);
         handlePrediction(prediction);
     }
-    else{
-     alert("Please choose a file first.") // this might be redundant as file is *required
+    else {
+        alert("Please Choose a file first")
+    }
+
+}
+
+
+//infer.js
+
+async function infer(formData){
+
+    const url = "http://127.0.0.1:8000/infer"
+
+    try{
+        const response = await fetch(
+            url,{
+                method: 'POST',
+                body: formData,
+            }
+        );
+
+        if (response.ok) {
+            return await response.json(); // return prediction result
+        }
+        else {
+            console.error('Error: ', response.statusText);
+        }
+
+    }catch (error) {
+        console.error('Error: ', error.message);
+    }
 }
 
 
-// await infer(result);
-
-}
-// async function infer(result){
-//     alert("infer");
-//     result.style.color = "red";
-// }
-
-
-
+//handle_prediction.js
